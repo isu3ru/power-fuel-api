939eeb87aa716b112ec57456988fca57c62f673c
Isuru Ranawaka <isu3ru@gmail.com>
2022-11-29 17:56:50 -0500

    composer.phar added

--- public/assets/js/app.js
+++ public/assets/js/app.js
@@ -7,9 +7,8 @@ var __webpack_exports__ = {};
   "use strict";
 
   var e,
-      t = localStorage.getItem("language"),
-      n = "en";
-
+    t = localStorage.getItem("language"),
+    n = "en";
   function a(e) {
     document.getElementById("header-lang-img") && ("en" == e ? document.getElementById("header-lang-img").src = "assets/images/flags/us.jpg" : "sp" == e ? document.getElementById("header-lang-img").src = "assets/images/flags/spain.jpg" : "gr" == e ? document.getElementById("header-lang-img").src = "assets/images/flags/germany.jpg" : "it" == e ? document.getElementById("header-lang-img").src = "assets/images/flags/italy.jpg" : "ru" == e && (document.getElementById("header-lang-img").src = "assets/images/flags/russia.jpg"), localStorage.setItem("language", e), null == (t = localStorage.getItem("language")) && a(n), s.getJSON("assets/lang/" + t + ".json", function (e) {
       s("html").attr("lang", t), s.each(e, function (e, t) {
@@ -17,21 +16,17 @@ var __webpack_exports__ = {};
       });
     }));
   }
-
   function c() {
     for (var e = document.getElementById("topnav-menu-content").getElementsByTagName("a"), t = 0, s = e.length; t < s; t++) {
       "nav-item dropdown active" === e[t].parentElement.getAttribute("class") && (e[t].parentElement.classList.remove("active"), null !== e[t].nextElementSibling && e[t].nextElementSibling.classList.remove("show"));
     }
   }
-
   function r(e) {
     1 == s("#light-mode-switch").prop("checked") && "light-mode-switch" === e ? (s("html").removeAttr("dir"), s("#dark-mode-switch").prop("checked", !1), s("#rtl-mode-switch").prop("checked", !1), s("#dark-rtl-mode-switch").prop("checked", !1), s("#bootstrap-style").attr("href", "assets/css/bootstrap.min.css"), s("#app-style").attr("href", "assets/css/app.min.css"), sessionStorage.setItem("is_visited", "light-mode-switch")) : 1 == s("#dark-mode-switch").prop("checked") && "dark-mode-switch" === e ? (s("html").removeAttr("dir"), s("#light-mode-switch").prop("checked", !1), s("#rtl-mode-switch").prop("checked", !1), s("#dark-rtl-mode-switch").prop("checked", !1), s("#bootstrap-style").attr("href", "assets/css/bootstrap-dark.min.css"), s("#app-style").attr("href", "assets/css/app-dark.min.css"), sessionStorage.setItem("is_visited", "dark-mode-switch")) : 1 == s("#rtl-mode-switch").prop("checked") && "rtl-mode-switch" === e ? (s("#light-mode-switch").prop("checked", !1), s("#dark-mode-switch").prop("checked", !1), s("#dark-rtl-mode-switch").prop("checked", !1), s("#bootstrap-style").attr("href", "assets/css/bootstrap-rtl.min.css"), s("#app-style").attr("href", "assets/css/app-rtl.min.css"), s("html").attr("dir", "rtl"), sessionStorage.setItem("is_visited", "rtl-mode-switch")) : 1 == s("#dark-rtl-mode-switch").prop("checked") && "dark-rtl-mode-switch" === e && (s("#light-mode-switch").prop("checked", !1), s("#rtl-mode-switch").prop("checked", !1), s("#dark-mode-switch").prop("checked", !1), s("#bootstrap-style").attr("href", "assets/css/bootstrap-dark-rtl.min.css"), s("#app-style").attr("href", "assets/css/app-dark-rtl.min.css"), s("html").attr("dir", "rtl"), sessionStorage.setItem("is_visited", "dark-rtl-mode-switch"));
   }
-
   function l() {
     document.webkitIsFullScreen || document.mozFullScreen || document.msFullscreenElement || (console.log("pressed"), s("body").removeClass("fullscreen-enable"));
   }
-
   s("#side-menu").metisMenu(), s("#vertical-menu-btn").on("click", function (e) {
     e.preventDefault(), s("body").toggleClass("sidebar-enable"), 992 <= s(window).width() ? s("body").toggleClass("vertical-collpsed") : s("body").removeClass("vertical-collpsed");
   }), s("#sidebar-menu a").each(function () {
@@ -58,7 +53,6 @@ var __webpack_exports__ = {};
           "#" === e.target.getAttribute("href") && (e.target.parentElement.classList.toggle("active"), e.target.nextElementSibling.classList.toggle("show"));
         };
       }
-
       window.addEventListener("resize", c);
     }
   }(), [].slice.call(document.querySelectorAll('[data-bs-toggle="tooltip"]')).map(function (e) {
@@ -91,7 +85,6 @@ var __webpack_exports__ = {};
         type = type || "json";
         onError = onError || undefined;
       }
-
       return s.ajax({
         url: url,
         type: method,
